a5587c2de39600eb2056df425264ba94724e4bf8
Teena kanawar <103328068+teena92084@users.noreply.github.com>
2023-07-22 17:02:14 +0530

    Add files via upload

---
+++ color_button.js
@@ -0,0 +1,31 @@
+
+const xyz=document.getElementsByClassName("xyz");
+
+const black_b=document.getElementById("black")
+black_b.onclick=function(){
+	xyz[0].style.backgroundColor="black";
+};				
+
+
+const green_b=document.getElementById("green")
+green_b.onclick=function(){
+	xyz[0].style.backgroundColor="green";
+};				
+
+const Red_b = document.getElementById("Red")
+Red_b.onclick=function(){
+	xyz[0].style.backgroundColor="Red";
+};				
+
+const blue_b=document.getElementById("blue")
+blue_b.onclick=function(){
+	xyz[0].style.backgroundColor="blue";
+};					
+			
+const yellow_b=document.getElementById("yellow")
+yellow_b.onclick=function(){
+	xyz[0].style.backgroundColor="yellow";
+};
+
+
+
